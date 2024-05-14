54d9411e35274401fed4c74cf45c328b7493877b
IvanPolitov Work <ivan.hanter21@mail.ru>
2024-05-14 16:36:06 +0500

    Stack

---
+++ LIFO_stack/Stack.py
@@ -0,0 +1,69 @@
+# Стек на связных списках
+class Stack:
+    _head = None
+
+    class Node:
+        def __init__(self, value, next=None):
+            self.value = value
+            self.next = next
+
+        def __str__(self):
+            return str(self.value)
+
+    def __init__(self, node=None):
+        if node is not None:
+            _node = self.Node(node)
+            self._head = _node
+
+    def __str__(self):
+        if self._head is None:
+            return '[]'
+        else:
+            output = ''
+            node = self._head
+            while node is not None:
+                output += str(node) + ' | '
+                node = node.next
+            output = output[:-2] + ']'
+            return output
+
+    def is_empty(self):
+        return self._head is None
+
+    def push(self, value):
+        node = self.Node(value)
+        if self.is_empty():
+            self._head = node
+        else:
+            node.next = self._head
+            self._head = node
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        else:
+            node = self._head
+            value = node.value
+            self._head = node.next
+            del node
+            return value
+
+    def top(self):
+        return self._head.value
+
+
+if __name__ == '__main__':
+    q = Stack()
+    q.push(1)
+    print(q)
+    q.push(2)
+    print(q)
+    q.push(3)
+    print(q.top())
+    print(q)
+    print(q.pop())
+    print(q)
+    print(q.pop())
+    print(q)
+    print(q.pop())
+    print(q)
---
+++ LIFO_stack/parentheses.py
@@ -0,0 +1,46 @@
+import unittest
+from Stack import Stack
+
+
+def parentheses(s):
+    par = {
+        ')': '(',
+        '}': '{',
+        ']': '[',
+    }
+    q = Stack()
+    for i, c in enumerate(s):
+        if c in '([{':
+            q.push((c, i))
+        elif c in ')]}':
+            if q.is_empty():
+                return i + 1
+            if par[c] == q.top()[0]:
+                q.pop()
+            else:
+                return i + 1
+    if q.is_empty():
+        return 'Success'
+    else:
+        return q.top()[1] + 1
+
+
+class TestParentheses(unittest.TestCase):
+    def test1(self):
+        self.assertEqual('Success', parentheses('([](){([])})'))
+
+    def test2(self):
+        self.assertEqual(1, parentheses('{'))
+
+    def test3(self):
+        self.assertEqual(7, parentheses('{{[()]]'))
+
+    def test4(self):
+        self.assertEqual(10, parentheses('foo(bar[i);'))
+
+    def test5(self):
+        self.assertEqual(3, parentheses('[]([]'))
+
+
+if __name__ == '__main__':
+    unittest.main()
